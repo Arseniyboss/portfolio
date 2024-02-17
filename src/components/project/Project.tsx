c2cf7590457826ec6853a8c1be407d3a1d506783
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-02-17 14:43:07 +0100

    optimize project images

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -7,7 +7,7 @@ const Project = ({ img, url, name }: Props) => {
   return (
     <Link href={url} target='_blank'>
       <Container>
-        <CardImage src={img} alt='' width={700} height={400} priority />
+        <CardImage src={img} alt='' width={432} height={345} priority />
         <ProjectName>{name}</ProjectName>
       </Container>
     </Link>
