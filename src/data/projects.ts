62cac4314d955adda8a65c0e8b2913572e694615
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-11-09 09:51:37 +0100

    reorder project images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,14 +2,14 @@ import { Project } from '@/types'
 
 const projects: Project[] = [
   {
-    name: 'E-Commerce',
-    url: 'https://ebuyshop.vercel.app',
-    img: '/projects/ecommerce.png',
+    name: 'CodePen Clone',
+    url: 'https://custom-codepen.netlify.app',
+    img: '/projects/codepen-clone.png',
   },
   {
-    name: 'Landing Page',
-    url: 'https://landing-page-boilerplate.netlify.app',
-    img: '/projects/landing-page.png',
+    name: 'Chat App',
+    url: 'https://general-chat.onrender.com',
+    img: '/projects/chat-app.png',
   },
   {
     name: 'Task Manager',
@@ -22,29 +22,29 @@ const projects: Project[] = [
     img: '/projects/project-manager.png',
   },
   {
-    name: 'CodePen Clone',
-    url: 'https://custom-codepen.netlify.app',
-    img: '/projects/codepen-clone.png',
-  },
-  {
-    name: 'Chat App',
-    url: 'https://general-chat.onrender.com',
-    img: '/projects/chat-app.png',
-  },
-  {
     name: 'Dashboard',
     url: 'https://custom-sales-dashboard.netlify.app',
     img: '/projects/dashboard.png',
   },
   {
-    name: '3D iPhone',
-    url: 'https://3d-iphone15.netlify.app',
-    img: '/projects/3d-iphone.png',
+    name: 'E-Commerce',
+    url: 'https://ebuyshop.vercel.app',
+    img: '/projects/ecommerce.png',
+  },
+  {
+    name: 'Landing Page',
+    url: 'https://landing-page-boilerplate.netlify.app',
+    img: '/projects/landing-page.png',
   },
   {
     name: 'Memory Game',
     url: 'https://card-matching-memory-game.netlify.app',
     img: '/projects/memory-game.png',
+  },
+  {
+    name: '3D iPhone',
+    url: 'https://3d-iphone15.netlify.app',
+    img: '/projects/3d-iphone.png',
   },
 ]
 
